ae5969c7b4e32b54235537ebb533795c9fd39587
yulondawyatt@gmail.com <yulondajoywyatt@Yulondas-MacBook-Air.local>
2023-08-11 13:05:36 -0400

    FINALLY completed this labgit status

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>Lab React Deployment Lab</div>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
